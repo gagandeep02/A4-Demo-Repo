4f40b145b97096fe36b4dba6c2892de5bdabfdd8
gagandeep02 <gdeep4033@gmail.com>
2020-03-30 12:07:05 +0530

    this is the project

---
+++ src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,18 @@
+package org.cap.demo;
+
+//import java.util.Arrays;
+//import java.util.List;
+
+public class MainClass 
+{
+	
+		public static void main(String[] args)
+		{
+			 int num1 = 5, num2 = 15, sum;
+		      sum = num1 + num2;
+
+		      System.out.println("Sum of these numbers: "+sum);
+
+		}
+
+}
